619891a87057ccae1f1c4cea635b8316bc859e44
Ben Cates <ben@system76.com>
2017-11-26 20:40:51 -0700

    Set up Ecto

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,17 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :winchester,
+  ecto_repos: [Winchester.Repo]
+
+config :winchester, Winchester.Repo,
+  adapter: Ecto.Adapters.MySQL,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  database: "winchester",
+  username: "winchester",
+  hostname: "localhost"
+
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
---
+++ lib/winchester/repo.ex
@@ -0,0 +1,3 @@
+defmodule Winchester.Repo do
+  use Ecto.Repo, otp_app: :winchester
+end
